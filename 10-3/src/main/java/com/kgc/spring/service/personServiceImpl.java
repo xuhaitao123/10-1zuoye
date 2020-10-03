0e269c673eac421dd9a92dc2863f395d62d0688a
xht123 <lzsshixd20020507>
2020-10-03 13:37:17 +0800

    10-3

---
+++ 10-3/src/main/java/com/kgc/spring/service/personServiceImpl.java
@@ -0,0 +1,27 @@
+package com.kgc.spring.service;
+
+import com.kgc.spring.entity.person;
+import com.kgc.spring.mapper.personMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+@Service("personservice")
+public class personServiceImpl implements personService {
+    @Resource
+    personMapper personMapper;
+    @Override
+    public List<person> select() {
+        return personMapper.selectByExample(null);
+    }
+
+    @Override
+    public int add(person person) {
+        return personMapper.insert(person);
+    }
+
+    @Override
+    public int deleteByPrimaryKey(Integer id) {
+        return personMapper.deleteByPrimaryKey(id);
+    }
+}
